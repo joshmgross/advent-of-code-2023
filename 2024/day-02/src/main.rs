9d91056fb022f62c41a3ab1a23f9d5308b564299
Josh Gross <joshuamgr@gmail.com>
2024-12-03 21:42:26 -0500

    Day 2 part 2 (not working yet)

--- 2024/day-02/src/main.rs
+++ 2024/day-02/src/main.rs
@@ -6,39 +6,72 @@ fn parse_levels(line: &str) -> Vec<i32> {
         .collect::<Vec<i32>>()
 }
 
-fn safe(report: Vec<i32>) -> bool {
+#[derive(PartialEq)]
+enum SafeResult {
+    Ok,
+    InvalidIndex(usize),
+}
+
+fn safe(report: &Vec<i32>) -> SafeResult {
     if report.len() < 2 {
-        return true;
+        return SafeResult::Ok;
     }
 
     let first = report[0];
     let second = report[1];
     if first == second {
-        return false;
+        return SafeResult::InvalidIndex(1);
     }
 
     let increasing = if second > first { true } else { false };
 
     let mut prev = first;
-    for level in report.into_iter().skip(1) {
+    for i in 1..report.len() {
+        let level = report[i];
         if increasing {
             if level < prev {
-                return false;
+                return SafeResult::InvalidIndex(i);
             }
         } else {
             if level > prev {
-                return false;
+                return SafeResult::InvalidIndex(i);
             }
         }
 
         if level.abs_diff(prev) < 1 || level.abs_diff(prev) > 3 {
-            return false;
+            return SafeResult::InvalidIndex(i);
         }
 
         prev = level;
     }
 
-    return true;
+    return SafeResult::Ok;
+}
+
+fn solve(reports: &Vec<Vec<i32>>, dampener: bool) -> usize {
+    let mut num_safe = 0;
+    let mut num_dampened_safe = 0;
+    for report in reports {
+        match safe(&report) {
+            SafeResult::Ok => num_safe += 1,
+            SafeResult::InvalidIndex(i) => {
+                if dampener {
+                    let mut new_report = report.clone();
+                    new_report.remove(i);
+
+                    let mut new_report_two = report.clone();
+                    new_report_two.remove(i - 1);
+                    if safe(&new_report) == SafeResult::Ok
+                        || safe(&new_report_two) == SafeResult::Ok
+                    {
+                        num_dampened_safe += 1
+                    }
+                }
+            }
+        }
+    }
+
+    return num_safe + num_dampened_safe;
 }
 
 fn main() {
@@ -46,11 +79,6 @@ fn main() {
 
     let reports = input.lines().map(parse_levels).collect::<Vec<Vec<i32>>>();
 
-    let mut num_safe = 0;
-    for report in reports {
-        if safe(report) {
-            num_safe += 1
-        }
-    }
-    println!("{}", num_safe);
+    println!("part 1: {}", solve(&reports, false));
+    println!("part 2: {}", solve(&reports, true));
 }
